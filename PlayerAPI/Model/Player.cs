d1e46910f3def9e5afd4235e58fe90a324fe1544
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-03-09 00:58:41 +0100

    first setup and Player model created

---
+++ PlayerAPI/Model/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace PlayerAPI.Model
+{
+    public class Player
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string? Nationality { get; set; }
+        public List<string> Positions { get; set; } = new List<string>();
+
+        public Player(string name, int age, string nationality, List<string> positions)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Nationality = nationality;
+            this.Positions = positions;
+
+        }
+    }
+}
